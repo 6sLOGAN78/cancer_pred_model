34a3f0c3404017fe2426759e077db91801e37a80
AyushMaurya-tech <ayush_2401ct23@iitp.ac.in>
2026-01-13 00:16:31 +0530

    pipeline1

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.utils.common import get_size
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+try:
+    logger.info(f">>>>>>>  stage {}
