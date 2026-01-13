fa73bcb4e18bbf508163ae02597e630f2a1abcb3
AyushMaurya-tech <ayush_2401ct23@iitp.ac.in>
2026-01-13 16:35:03 +0530

    done

--- src/cnnClassifier/components/model_evaluation_mlflow.py
+++ src/cnnClassifier/components/model_evaluation_mlflow.py
@@ -4,18 +4,20 @@ import mlflow
 import mlflow.keras
 from urllib.parse import urlparse
 from cnnClassifier.entity.config_entity import EvaluationConfig
-from cnnClassifier.utils.common import read_yaml, create_directories,save_json
+from cnnClassifier.utils.common import save_json
 
 
 class Evaluation:
     def __init__(self, config: EvaluationConfig):
         self.config = config
 
-    
+    # -------------------------------
+    # Create validation data generator
+    # -------------------------------
     def _valid_generator(self):
 
         datagenerator_kwargs = dict(
-            rescale = 1./255,
+            rescale=1.0 / 255,
             validation_split=0.30
         )
 
@@ -36,39 +38,55 @@ class Evaluation:
             **dataflow_kwargs
         )
 
-
+    # -------------------------------
+    # Load trained model
+    # -------------------------------
     @staticmethod
     def load_model(path: Path) -> tf.keras.Model:
         return tf.keras.models.load_model(path)
-    
 
+    # -------------------------------
+    # Run evaluation
+    # -------------------------------
     def evaluation(self):
         self.model = self.load_model(self.config.path_of_model)
         self._valid_generator()
         self.score = self.model.evaluate(self.valid_generator)
-        self.save_score()
 
+        self.save_score()
+        self.log_into_mlflow()   # 🔥 VERY IMPORTANT
+
+    # -------------------------------
+    # Save metrics locally
+    # -------------------------------
     def save_score(self):
-        scores = {"loss": self.score[0], "accuracy": self.score[1]}
+        scores = {
+            "loss": float(self.score[0]),
+            "accuracy": float(self.score[1])
+        }
         save_json(path=Path("scores.json"), data=scores)
 
-    
+    # -------------------------------
+    # Log everything to DagsHub MLflow
+    # -------------------------------
     def log_into_mlflow(self):
-        mlflow.set_registry_uri(self.config.mlflow_uri)
+        mlflow.set_tracking_uri(self.config.mlflow_uri)
+
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-        
+        print("MLflow Tracking URI:", mlflow.get_tracking_uri())
+
         with mlflow.start_run():
             mlflow.log_params(self.config.all_params)
-            mlflow.log_metrics(
-                {"loss": self.score[0], "accuracy": self.score[1]}
-            )
-            # Model registry does not work with file store
-            if tracking_url_type_store != "file":
+            mlflow.log_metrics({
+                "loss": self.score[0],
+                "accuracy": self.score[1]
+            })
 
-                # Register the model
-                # There are other ways to use the Model Registry, which depends on the use case,
-                # please refer to the doc for more information:
-                # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                mlflow.keras.log_model(self.model, "model", registered_model_name="VGG16Model")
+            if tracking_url_type_store != "file":
+                mlflow.keras.log_model(
+                    self.model,
+                    "model",
+                    registered_model_name="VGG16Model"
+                )
             else:
                 mlflow.keras.log_model(self.model, "model")
