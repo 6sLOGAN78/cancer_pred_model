4575d9459091b0df347b52045975044105a3d21e
AyushMaurya-tech <ayush_2401ct23@iitp.ac.in>
2026-01-13 14:47:32 +0530

    prepared based model

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ from cnnClassifier import logger
 from cnnClassifier.utils.common import get_size
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-
+STAGE_NAME = "Data Ingestion stage"
 try:
-    logger.info(f">>>>>>>  stage {}
+    logger.info(f">>>>>>> stage {STAGE_NAME}")
+    obj= DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>  stage {STAGE_NAME} completed <<<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
